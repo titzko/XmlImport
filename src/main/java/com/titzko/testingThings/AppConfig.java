865d4c89534c3375117b6375a7ea5025e44448e0
titzko <71842830+titzko@users.noreply.github.com>
2021-05-18 13:44:40 +0200

    Update AppConfig.java

--- src/main/java/com/titzko/testingThings/AppConfig.java
+++ src/main/java/com/titzko/testingThings/AppConfig.java
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
 
 @Configuration
 public class AppConfig {
-    private static final String DIRECTORY="C:/Users/matthias.willers/Desktop/projectXmlImportDirectory";
+    private static final String DIRECTORY="";
 
 
     @Autowired
